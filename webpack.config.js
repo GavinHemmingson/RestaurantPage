2b1533cc6f884891e2d6f7241c6eb432f8f4f32f
gavinhemmingson <gavinhemmingson@gmail.com>
2024-06-15 16:28:52 -0500

    package-lock.json

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            title: 'Restaurant Page',
+            template:'./src/index.html',
+            minify:false,
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+};
